b058dcfeea82fe5d3e1726727b94ea9053a57533
semblant <robynhumphreys7_4@hotmail.com>
2024-09-28 12:45:12 -0600

    created assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`❌❌❌ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(3, 4);
+assertEqual(1, 1);
