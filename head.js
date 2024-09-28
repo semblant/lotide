36262a2b9c4762131223317616377ed89539a62d
semblant <robynhumphreys7_4@hotmail.com>
2024-09-28 13:32:14 -0600

    improved test case comments

--- head.js
+++ head.js
@@ -10,10 +10,10 @@ const head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5, 6, 7]), 5); // compare identical nums
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // compare identical strings
-assertEqual(head([0,1,2]), 8); // compare non-identical nums
-assertEqual(head(["Hello"]), "Bootcamp"); // compare 1-element array to non-identical string
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), ""); // compare array to empty string
-assertEqual(head([]), 5); // compare empty array to num
-assertEqual(head([]), ""); // compare empty array to empty string
+assertEqual(head([5, 6, 7]), 5); // Test Case: Checking if first element is 5
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Test case: checking if first element is 'Hello'
+assertEqual(head([0,1,2]), 8); // Test case: checking if first element is 8
+assertEqual(head(["Hello"]), "Bootcamp"); // Test case: checking if first element is 'Bootcamp'
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), ""); // Test case: checking if first element is empty string ""
+assertEqual(head([]), 5); // Test Case: checking if first element of an empty array is 5
+assertEqual(head([]), ""); // Test case: checking if first element in empty array is an empty string ""
