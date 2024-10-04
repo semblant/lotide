f318ccd8d4ebe8ac8dd1f3ecc4aacac79208c113
semblant <robynhumphreys7_4@hotmail.com>
2024-10-03 22:16:21 -0600

    Improving syntax and removing unnecessary code

--- eqObjects.js
+++ eqObjects.js
@@ -1,23 +1,16 @@
 
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
+  if (actual === expected) console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length  !== arr2.length) { // check if arrays are the same length, else return false.
-    return false; // exit loop if not equal in length
-  }
+  if (arr1.length  !== arr2.length) return false; // exit loop if not equal in length
   for (let i = 0; i < arr1.length; i++) { // loop through all elements
     // only need to use arr1.length as the condition since they should be the same length
-    if (arr1[i] !== arr2[i]) {
-      return false; // exit loop if values aren't equal
+    if (arr1[i] !== arr2[i]) return false; // exit loop if values aren't equal
     }
-  }
-  return true; // return true after checking all elements
+    return true; // return true after checking all elements
 };
 
 
@@ -26,24 +19,15 @@ const eqArrays = function(arr1, arr2) {
 const eqObjects = function(object1, object2) {
   const keys1 = Object.keys(object1); // store object1 keys as array
   const keys2 = Object.keys(object2); // store object2 keys as array
-  let matchFlag = false;
-  if (keys1.length !== keys2.length ) { // case: check if objects are the same length
-    return false; // return false if objects aren't same length
-  } else {
+  if (keys1.length !== keys2.length ) return false; // return false if object key arrays are not same length
+  else {
     for (let key of keys1) { // get the keys from any object
-      if (!object2.hasOwnProperty(key)) { // if key from object1 doesnt match key from object2 return false
-        return false; // if no property, return false
-        } else if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { // check if the key is an array
-          if (eqArrays(object1[key], object2[key]) === false) {
-            return false;
-          }
-          } else if (object1[key] !== object2[key]) { // both objects should have the key, check if values are the same
-          return false; // if values don't match return false
-          } else {
-
-          }
+      if (!object2.hasOwnProperty(key)) return false; // if key from object1 doesnt match key from object2 return false
+      else if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { // check if the key is an array
+        if (eqArrays(object1[key], object2[key]) === false) return false;
+      } else if (object1[key] !== object2[key]) return false; // both objects should have the key, check if values are the same
+      return true;
     }
-    return true;
   }
 };
 // Test Case: Same keys and values in different positions
