94f54aa64f886171854f4f0f59b1db0fdca7885d
semblant <robynhumphreys7_4@hotmail.com>
2024-10-17 23:29:44 -0600

    Refactored middle to export the function, made test file to require the function.

--- middle.js
+++ middle.js
@@ -1,41 +1,3 @@
-/* CHALLENGE
-* Implement middle which will take in an array and return the middle-most element(s) of the given array.
-* 1. Arrays with 1, 2 elements should return an empty array
-* 2. Odd numbered arrays should return one element.
-* 3. Even numbered arrays should return two elements in the middle.
-*/
-
-/*
-
-* Function eqArrays(): evaluates if two arrays are identical
-* Checks if length of 2 arrays are identical, returns true or false
-*/
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length  !== arr2.length) { // check if arrays are the same length, else return false.
-    return false; // exit loop if not equal in length
-  }
-  for (let i = 0; i < arr1.length; i++) { // loop through all elements
-    // only need to use arr1.length as the condition since they should be the same length
-    if (arr1[i] !== arr2[i]) {
-      return false; // exit loop if values aren't equal
-    }
-  }
-  return true; // return true after checking all elements
-};
-
-/*
-* Function assertArraysEqual(): calls eqArrays function and prints to the console if the assertion has passed or failed.
-* Calls eqArrays() which evaluates if the arrays are identical
-* Console logs if assertion has passed or failed.
-*/
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1,arr2)) { // if the return value is true
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {// if return value is NOT true
-    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 /*
 Function middle(): takes an array and returns the middle number(s) as an array
 * Checks if the length has three elements, returns an empty array.
@@ -57,12 +19,4 @@ const middle = function(arr) {
   return midArr;
 };
 
-assertArraysEqual(middle([1]), []); // => 2-element array should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3, 4, 5]); // => 5 element array should FAIL
-assertArraysEqual(middle([1, 2, 4, 10, 14, 1]), [4, 10]); // => 6 element array should PASS
-assertArraysEqual(middle([1, 2, "Hello", 10, 14]), ["Hello"]); // 5 element array with text should PASS
-
-// Test Case: check the original array has not been changed.
-const testArr = [1, 2, 3, 'Hello', 4, 6];
-middle(testArr);
-assertArraysEqual(testArr, [1, 2, 3, 'Hello', 4, 6]);
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+assertArraysEqual(middle([1]), []); // Test Case: checking if 2-element array returns ''
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Test Case: Checking that a 5-element array returns the middle element
+assertArraysEqual(middle([1, 2, 4, 10, 14, 1]), [4, 10]); // // Test Case: Checking that a 6-element array returns the two middle elements
+assertArraysEqual(middle([1, 2, "Hello", 10, 14]), ["Hello"]); // Test Case: Checking that a 5-element array with tect returns the middle element
+
+// Test Case: check the original array has not been changed.
+const testArr = [1, 2, 3, 'Hello', 4, 6];
+middle(testArr);
+assertArraysEqual(testArr, [1, 2, 3, 'Hello', 4, 6]);
