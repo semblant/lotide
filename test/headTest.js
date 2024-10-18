821a6cf67ed2ade99a182acd9040b8ed2637ef8a
semblant <robynhumphreys7_4@hotmail.com>
2024-10-17 23:44:13 -0600

    updating comments on tests

--- test/headTest.js
+++ test/headTest.js
@@ -3,18 +3,22 @@ const head   = require('../head');
 
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
+    // Test case: head function with a normal array
     assert.strictEqual(head([1, 2, 3]), 1);
   });
 
   it("returns '5' for ['5']", () => {
+    // Test case: head function with a single element array
     assert.strictEqual(head(['5']), '5');
   });
 
   it("returns '&' for ['&','@','$']", () => {
+    // Test case: head function with an array of special characters
     assert.strictEqual(head(['&','@','$']), '&');
   });
 
   it("returns 'undefined' for []", () => {
+    // Test case: head function with an empty array
     assert.strictEqual(head([]), undefined);
   });
 
