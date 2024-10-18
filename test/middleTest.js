14d466919d1ae16cb0c34c263b62fe5a4f0e11e0
semblant <robynhumphreys7_4@hotmail.com>
2024-10-18 00:06:13 -0600

    Updated testing to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,38 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const chai = require('chai');
+const assert = chai.assert;
 
-assertArraysEqual(middle([1,2]), []); // Test Case: checking if 2-element array returns ''
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // Test Case: Checking that a 5-element array returns the middle element
-assertArraysEqual(middle([1, 2, 4, 10, 14, 1]), [4, 10]); // // Test Case: Checking that a 6-element array returns the two middle elements
-assertArraysEqual(middle([1, 2, "Hello", 10, 14]), ["Hello"]); // Test Case: Checking that a 5-element array with tect returns the middle element
 
-// Test Case: check the original array has not been changed.
-const testArr = [1, 2, 3, 'Hello', 4, 6];
-middle(testArr);
-assertArraysEqual(testArr, [1, 2, 3, 'Hello', 4, 6]);
+describe("#middle", () => {
+  it("returns [] for [1, 2]", () => {
+    // Test Case: Checking that a 2-element array returns an empty array
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    // Test Case: Checking that a 5-element array returns the middle element
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns '&' for ['&','@','$']", () => {
+    // Test Case: Checking that a 6-element array returns the two middle elements
+    assert.deepEqual(middle([1, 2, 4, 10, 14, 1]), [4, 10]);
+  });
+
+  it("returns 'Hello' for [1, 2, 'Hello', 10, 14]", () => {
+    // Test Case: Checking that a 5-element array with tect returns the middle element
+    assert.deepEqual(middle([1, 2, "Hello", 10, 14]), ["Hello"]);
+  });
+
+  it("returns [1, 2, 3, 'Hello', 4, 6] for [1, 2, 3, 'Hello', 4, 6]", () => {
+    // Test Case: check the original array has not been changed.
+    const testArr = [1, 2, 'Hello', 4, 6];
+    middle(testArr);
+    assert.deepEqual(testArr, [1, 2, "Hello", 4, 6]);
+  });
+
+
+
+
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,27 +1,40 @@
-const assertEqual = require('../assertEqual')
-const tail = require('../tail')
+const chai = require('chai');
+const tail = require('../tail');
+const assert = chai.assert;
 
-// Test case 1: testing the equality of 2 string arrays. It is always false. Need to find a workaround.
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    // Test Case: Testing the equality of 2 string arrays.  It is always false. Need to find a workaround.
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']);
+  });
 
-// Test case 2: Check returned array elements of string array.
-const resultStringArray = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(resultStringArray.length, 2); // expecting two elements
-assertEqual(resultStringArray[0], "Lighthouse"); // expecting first element to be "Lighthouse"
-assertEqual(resultStringArray[1], "Labs"); // expecting second element to be "Labs"
+  it("returns 2 for ['Hello', 'Lighthouse', 'Labs'].length", () => {
+    // Test case 2: Check returned array elements of string array.
+    const resultStringArray = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(resultStringArray.length, 2);
+    assert.deepEqual(resultStringArray[0], "Lighthouse");
+    assert.deepEqual(resultStringArray[1], "Labs");
+  });
 
-// Test case 3: Check returned array elements of empty array
-const resultEmptyArray = tail([]);
-assertEqual(resultEmptyArray.length, 0); // expecting 0 elements
-assertEqual(resultEmptyArray[0], undefined); // expecting first element to be undefined
+  it("returns 'undefined' for []", () => {
+    // Test case: Check returned array elements of empty array
+    const resultEmptyArray = tail([]);
+    assert.deepEqual(resultEmptyArray.length, 0);
+    assert.deepEqual(resultEmptyArray[0], undefined);
+  });
 
-// Test case 4: Check returned array elements of 1-element array
-const resultOneElementArray = tail(["Hello"]);
-assertEqual(resultOneElementArray.length, 0); // expect array length to be 0
-assertEqual(resultOneElementArray[0], undefined); // expecting first element to be undefined.
+  it("returns 'undefined' for ['Hello']", () => {
+    // Test case: Check returned array elements of 1-element array
+    const resultOneElementArray = tail(["Hello"]);
+    assert.deepEqual(resultOneElementArray.length, 0);
+    assert.deepEqual(resultOneElementArray[0], undefined);
+  });
 
-// Test case 3: check if original array is not modified
-const items = ["phone", "computer", "charger"]; // new array
-tail(items); // seeing if tail affects the array items at all
-assertEqual(items.length, 3); // after running tail on items, it should still have 3 items.
+  it('returns ["phone", "computer", "charger"] for ["phone", "computer", "charger"]', () => {
+    // Test case: check if original array is not modified
+    const items = ["phone", "computer", "charger"];
+    tail(items);
+    assert.deepEqual(items.length, 3);
+
+  });
+});
