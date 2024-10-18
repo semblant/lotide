4bc0e05857c02c0b78d70105580bf5eb9b38fe75
semblant <robynhumphreys7_4@hotmail.com>
2024-10-17 23:07:56 -0600

    Fixing mistake (false instead of true) in test case 2 and 3.

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,6 +2,6 @@ const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // Test Case: Checking if 2 arrays of the same numbers are equal
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // Test Case: Ensuring that 2 arrays of different numbers fails
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), true); // Test Case: Ensuring that 2 arrays with different string/number combos fails
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // Test Case: Ensuring that 2 arrays of different numbers fails
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false); // Test Case: Ensuring that 2 arrays with different string/number combos fails
 assertEqual(eqArrays(['1', '2', '3',], ['1', '2', '3']), true); // Test Case: Checking that 2 arrays of the same strings are equal
