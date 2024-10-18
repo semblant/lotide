ffd4daba73de66ccfee0ac7bb04678b549deeaf8
semblant <robynhumphreys7_4@hotmail.com>
2024-10-17 23:42:31 -0600

    using Mocha & Chai for testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,21 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5, 6, 7]), 5); // Test Case: Checking if first element is 5
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Test case: checking if first element is 'Hello'
-assertEqual(head(['&','@','$']), '&'); // Test case: checking if first element is & - how does the function handle symbols
-assertEqual(head([]), undefined); // Test case: checking that an empty array returns undefined
-assertEqual(head([6, 7, 8]), 5); // Test Case: Ensuring that if the first element isn't 5, it fails
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns '&' for ['&','@','$']", () => {
+    assert.strictEqual(head(['&','@','$']), '&');
+  });
+
+  it("returns 'undefined' for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+});
